cb5914a245afdb01971f7db6bad30398dd223dfa
Sergio Leite <sergioleitedev@gmail.com>
2024-09-07 22:59:55 -0300

    🏗️ Initial Architecture. Folder Structure, State Management, Navigation, and Dependency Injection.

---
+++ lib/application/route_manager.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import 'package:my_movies/modules/home/home_bindings.dart';
+import 'package:my_movies/modules/home/view/home_page.dart';
+
+class RouteManager {
+  static String home = "/";
+
+  static List<GetPage> routes = [
+    GetPage(
+      name: home,
+      binding: HomeBindings(),
+      page: () => const HomePage(),
+    ),
+  ];
+}
