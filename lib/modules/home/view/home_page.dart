5ef54790970c2ee19c5576b1c762cc6619f03868
Sergio Leite <sergioleitedev@gmail.com>
2024-09-12 19:42:43 -0300

    ✨ feat: Tap to dismiss keyboard.

--- lib/modules/home/view/home_page.dart
+++ lib/modules/home/view/home_page.dart
@@ -17,94 +17,97 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      // appBar: AppBar(
-      //   centerTitle: true,
-      //   title: const Text("Trending"),
-      //   backgroundColor: SharedColors.favoritesRed,
-      //   foregroundColor: Colors.white,
-      // ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(20, 50, 20, 0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              children: [
-                const GetToFavoritesButton(),
-                const SizedBox(width: 16),
-                SearchTextField(),
-                SearchButton(
-                  onPressed: () {
-                    _searchController.onTapSearchButton();
-                    Get.toNamed(RouteManager.search);
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            GetBuilder<HomeController>(
-              builder: (_) {
-                return ToggleButtons(
-                  isSelected: _homeController.windowSelection,
-                  onPressed: (int index) => _homeController.toggleTrendingWindow(index),
-                  constraints: const BoxConstraints(minHeight: 0),
-                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  borderRadius: BorderRadius.circular(20),
-                  selectedColor: SharedColors.favoritesRed,
-                  fillColor: SharedColors.favoritesRed.withOpacity(0.22),
-                  children: const <Widget>[
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(15, 8, 8, 8),
-                      child: Text("Today"),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(8, 8, 15, 8),
-                      child: Text("This Week"),
-                    ),
-                  ],
-                );
-              },
-            ),
-            const SizedBox(height: 10),
-            const Expanded(child: MoviesGridView()),
-            GetBuilder<HomeController>(
-              builder: (_) {
-                if (_homeController.isLoading) {
-                  return Container(
-                    margin: const EdgeInsets.fromLTRB(0, 8, 0, 8),
-                    height: 20,
-                    width: 20,
-                    child: const CircularProgressIndicator(
-                      color: SharedColors.favoritesRed,
-                    ),
-                  );
-                }
-                return const SizedBox();
-              },
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: GetBuilder<HomeController>(
-        builder: (_) {
-          if (_homeController.shouldDisplayBackToTop) {
-            return FloatingActionButton(
-              onPressed: _homeController.backToTop,
-              backgroundColor: Colors.white.withOpacity(0.82),
-              foregroundColor: SharedColors.favoritesRed,
-              mini: true,
-              child: const Icon(
-                Icons.arrow_upward,
-                size: 30,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        // appBar: AppBar(
+        //   centerTitle: true,
+        //   title: const Text("Trending"),
+        //   backgroundColor: SharedColors.favoritesRed,
+        //   foregroundColor: Colors.white,
+        // ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 50, 20, 0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  const GetToFavoritesButton(),
+                  const SizedBox(width: 16),
+                  SearchTextField(),
+                  SearchButton(
+                    onPressed: () {
+                      _searchController.onTapSearchButton();
+                      Get.toNamed(RouteManager.search);
+                    },
+                  ),
+                ],
               ),
-            );
-          }
-          return const SizedBox();
-        },
+              const SizedBox(height: 10),
+              GetBuilder<HomeController>(
+                builder: (_) {
+                  return ToggleButtons(
+                    isSelected: _homeController.windowSelection,
+                    onPressed: (int index) => _homeController.toggleTrendingWindow(index),
+                    constraints: const BoxConstraints(minHeight: 0),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    borderRadius: BorderRadius.circular(20),
+                    selectedColor: SharedColors.favoritesRed,
+                    fillColor: SharedColors.favoritesRed.withOpacity(0.22),
+                    children: const <Widget>[
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(15, 8, 8, 8),
+                        child: Text("Today"),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(8, 8, 15, 8),
+                        child: Text("This Week"),
+                      ),
+                    ],
+                  );
+                },
+              ),
+              const SizedBox(height: 10),
+              const Expanded(child: MoviesGridView()),
+              GetBuilder<HomeController>(
+                builder: (_) {
+                  if (_homeController.isLoading) {
+                    return Container(
+                      margin: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                      height: 20,
+                      width: 20,
+                      child: const CircularProgressIndicator(
+                        color: SharedColors.favoritesRed,
+                      ),
+                    );
+                  }
+                  return const SizedBox();
+                },
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: GetBuilder<HomeController>(
+          builder: (_) {
+            if (_homeController.shouldDisplayBackToTop) {
+              return FloatingActionButton(
+                onPressed: _homeController.backToTop,
+                backgroundColor: Colors.white.withOpacity(0.82),
+                foregroundColor: SharedColors.favoritesRed,
+                mini: true,
+                child: const Icon(
+                  Icons.arrow_upward,
+                  size: 30,
+                ),
+              );
+            }
+            return const SizedBox();
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
     );
   }
 }
--- lib/modules/search/view/search_results_page.dart
+++ lib/modules/search/view/search_results_page.dart
@@ -14,68 +14,71 @@ class SearchResultsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Search Results"),
-        backgroundColor: SharedColors.favoritesRed,
-        foregroundColor: Colors.white,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(20, 15, 20, 0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              children: [
-                const GetToFavoritesButton(),
-                const SizedBox(width: 16),
-                SearchTextField(),
-                SearchButton(
-                  onPressed: _controller.onTapSearchButton,
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            const Expanded(child: MoviesListView()),
-            GetBuilder<SearchMoviesController>(
-              builder: (_) {
-                if (_controller.isLoading) {
-                  return Container(
-                    margin: const EdgeInsets.fromLTRB(0, 8, 0, 8),
-                    height: 20,
-                    width: 20,
-                    child: const CircularProgressIndicator(
-                      color: SharedColors.favoritesRed,
-                    ),
-                  );
-                }
-                return const SizedBox();
-              },
-            ),
-            //const SizedBox(height: 10),
-          ],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Search Results"),
+          backgroundColor: SharedColors.favoritesRed,
+          foregroundColor: Colors.white,
         ),
-      ),
-      floatingActionButton: GetBuilder<SearchMoviesController>(
-        builder: (_) {
-          if (_controller.shouldDisplayBackToTop) {
-            return FloatingActionButton(
-              onPressed: _controller.backToTop,
-              backgroundColor: Colors.white.withOpacity(0.82),
-              foregroundColor: SharedColors.favoritesRed,
-              mini: true,
-              child: const Icon(
-                Icons.arrow_upward,
-                size: 30,
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 15, 20, 0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  const GetToFavoritesButton(),
+                  const SizedBox(width: 16),
+                  SearchTextField(),
+                  SearchButton(
+                    onPressed: _controller.onTapSearchButton,
+                  ),
+                ],
               ),
-            );
-          }
-          return const SizedBox();
-        },
+              const SizedBox(height: 10),
+              const Expanded(child: MoviesListView()),
+              GetBuilder<SearchMoviesController>(
+                builder: (_) {
+                  if (_controller.isLoading) {
+                    return Container(
+                      margin: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                      height: 20,
+                      width: 20,
+                      child: const CircularProgressIndicator(
+                        color: SharedColors.favoritesRed,
+                      ),
+                    );
+                  }
+                  return const SizedBox();
+                },
+              ),
+              //const SizedBox(height: 10),
+            ],
+          ),
+        ),
+        floatingActionButton: GetBuilder<SearchMoviesController>(
+          builder: (_) {
+            if (_controller.shouldDisplayBackToTop) {
+              return FloatingActionButton(
+                onPressed: _controller.backToTop,
+                backgroundColor: Colors.white.withOpacity(0.82),
+                foregroundColor: SharedColors.favoritesRed,
+                mini: true,
+                child: const Icon(
+                  Icons.arrow_upward,
+                  size: 30,
+                ),
+              );
+            }
+            return const SizedBox();
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
     );
   }
 }
